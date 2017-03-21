c14ddac4a04f0c7e6b309476cc2c37427bb1f220
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-21 11:50:44 +0100

    fixing folder add

---
+++ week-02/day-02/src/HelloOthers.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hegyi on 2017-03-21.
+ */
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Ramir!");
+        System.out.println("Hello, Norbert!");
+        System.out.println("Hello, Marci");
+    }
+}
---
+++ week-02/day-02/src/IntroduceYourself.java
@@ -0,0 +1,12 @@
+/**
+ * Created by hegyi on 2017-03-21.
+ */
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Viktor Hegyi");
+        System.out.println(31);
+        System.out.println(1.83);
+
+    }
+
+}
