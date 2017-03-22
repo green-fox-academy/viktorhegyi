b883d2c93d8ad1c3afd61687009288ddc5e50e7c
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-22 11:37:17 +0100

    add doubling.java

---
+++ week-02/day-03/src/Doubling.java
@@ -0,0 +1,18 @@
+/**
+ * Created by hegyi on 2017-03-22.
+ */
+public class Doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+
+    doubling(ak);
+
+  }
+  public static void doubling(int ak) {
+    ak = ak * 2;
+    System.out.println(ak);
+    }
+}
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Print the result of `doubling(ak)`
