52e0727063cfcca000d4c99948b556ff7eb64670
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-22 16:57:58 +0100

    add colors.java

---
+++ week-02/day-03/src/Colors.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * Created by hegyi on 2017-03-22.
+ */
+public class Colors {
+  public static void main(String[] args) {
+    String[][] colors = {
+            {"lime", "forest green", "olive", "pale green", "spring green"},
+            {"orange", "red", "red", "tomato"},
+            {"orchid", "violet", "pink", "hot pink"}
+            };
+    System.out.println(Arrays.toString(colors[0]));
+    System.out.println(Arrays.toString(colors[1]));
+    System.out.println(Arrays.toString(colors[2]));
+
+
+  }
+  }
+
+
+// - Create a two dimensional array
+//   which can contain the different shades of specified colors
+// - In `colors[0]` store the shades of green:
+//   `"lime", "forest green", "olive", "pale green", "spring green"`
+// - In `colors[1]` store the shades of red:
+//   `"orange red", "red", "tomato"`
+// - In `colors[2]` store the shades of pink:
+//   `"orchid", "violet", "pink", "hot pink"`
