4716ffccd02acea921da96fbc036c25aad2ee754
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-22 15:44:18 +0100

    add printall.java

---
+++ week-02/day-03/src/PrintAll.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * Created by hegyi on 2017-03-22.
+ */
+public class PrintAll {
+  public static void main(String[] args) {
+    int[] af = {4, 5, 6, 7};
+
+    System.out.println(Arrays.toString(af));
+
+  }
+}
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
