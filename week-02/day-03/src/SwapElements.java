8b35d1356f5fa08efc50a5bd615418169cf9ae24
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-22 19:41:29 +0100

    finish swapelements.java

--- week-02/day-03/src/SwapElements.java
+++ week-02/day-03/src/SwapElements.java
@@ -7,9 +7,10 @@ public class SwapElements {
   public static void main(String[] args) {
     String [] abc = {"first", "second", "third"};
 
+    String swap = abc[0];
     abc[0] = abc[2];
-    abc[2] = abc[0];
-
+    abc[2] = swap;
+    
     System.out.println(Arrays.toString(abc));
 
   }
