a4202a0ee32d1abd2173d1540891ca55a17e6b6c
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-22 17:13:17 +0100

    add appenda2.java

---
+++ week-02/day-03/src/AppendA2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+/**
+ * Created by hegyi on 2017-03-22.
+ */
+public class AppendA2 {
+  public static void main(String[] args) {
+    String [] nimals = {"kuty", "macs", "cic"};
+
+    for ( int i = 0; i < nimals.length; i++) {
+        nimals[i] +="a";
+    }
+    
+    System.out.println(Arrays.toString(nimals));
+  }
+}
+// - Create an array variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
