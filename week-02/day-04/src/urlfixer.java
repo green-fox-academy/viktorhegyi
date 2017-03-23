36ece7f31501f5de8610db0b8b6d243f0e50e17e
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-23 12:17:05 +0100

    add urlfixer.java

---
+++ week-02/day-04/src/urlfixer.java
@@ -0,0 +1,22 @@
+/**
+ * Created by hegyi on 2017-03-23.
+ */
+public class urlfixer {
+  public static void main(String... args){
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+
+    url = url.replace("bots", "odds");
+
+    String fixedUrlFirstPart = url.substring(0, 5);
+    String fixedUrlSecondPart = url. substring(5);
+
+    url = fixedUrlFirstPart + ":" + fixedUrlSecondPart;
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crutial component, find out what it is and insert it too!
+    // Try to solve it more than once using different String functions!
+
+    System.out.println(url);
+  }
+
+}
