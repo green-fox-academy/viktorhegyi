33bdc28de3fba52207cadd50421a96110bd049f4
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-28 11:41:27 +0200

    add centeredsquare.java

---
+++ week-03/day-02/src/CenteredSquare.java
@@ -0,0 +1,39 @@
+/**
+ * Created by hegyi on 2017-03-28.
+ */
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenteredSquare {
+
+  public static void mainDraw(Graphics graphics){
+
+    graphics.setColor(Color.GREEN);
+    graphics.drawRect(145, 145, 10,10);
+
+    // draw a green 10x10 square to the canvas' center.
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
