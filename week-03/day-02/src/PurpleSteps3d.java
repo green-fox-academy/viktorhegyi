e1df10447304907f78dd9ff35ae3bfbec3fbc60b
Viktor Hegyi <viktorhegyi@gmail.com>
2017-03-28 14:42:16 +0200

    add purplesteps3d.java, add function to purplesteps.java

--- week-03/day-02/src/PurpleSteps3d.java
+++ week-03/day-02/src/PurpleSteps3d.java
@@ -7,24 +7,23 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class PurpleSteps {
+public class PurpleSteps3d {
 
   public static void mainDraw(Graphics graphics){
 
     draw(graphics);
 
     // reproduce this:
-    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/purple-steps/r3.png]
-
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/purple-steps-3d/r4.png]
   }
 
   private static void draw( Graphics graphics) {
 
-    for (int i = 10; i < 200 ; i += 10) {
+    for (int i = 10; i < 200 ; i *= 2) {
       graphics.setColor(new Color(201,0,245));
-      graphics.fillRect(i,i,10,10);
+      graphics.fillRect(i,i,i,i);
       graphics.setColor(Color.BLACK);
-      graphics.drawRect(i,i,10,10);
+      graphics.drawRect(i,i,i,i);
     }
   }
 
