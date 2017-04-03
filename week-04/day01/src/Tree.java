f8134a5d3c147804f535f7c7e116d00da909498f
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-03 08:59:30 +0200

    add Tree.java

---
+++ week-04/day01/src/Tree.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * Created by hegyi on 2017-04-02.
+ */
+public class Tree {
+
+  public static void main(String[] args) {
+
+    String[] treeOne = {"Nothern red Oak", "red", "20", "female"};
+    String[] treeTwo ={"europian beech", "green", "23", "male"};
+    String[] treeThree ={"stone pine", "green", "30", "male"};
+    String[] treeFour ={"golden wattle", "green", "35", "female"};
+    String[] treeFive ={"red maple", "red", "40", "male"};
+
+  }
+
+}
