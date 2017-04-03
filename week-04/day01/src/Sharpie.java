c7dad3f5e5fd208aecdaabd23aa7d81a1cf5997d
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-03 15:49:29 +0200

    add Sharpie.java

---
+++ week-04/day01/src/Sharpie.java
@@ -0,0 +1,19 @@
+/**
+ * Created by hegyi on 2017-04-03.
+ */
+public class Sharpie {
+
+  String color;
+  float width;
+  float inkAmount;
+
+  public Sharpie(String color, float width) {
+    this.color = color;
+    this.width = width;
+    inkAmount = 100;
+  }
+
+  public void use() {
+    inkAmount--;
+  }
+}
