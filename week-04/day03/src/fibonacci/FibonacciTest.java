525397ca2666226edb5aef87fe1b60173627ae84
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-05 14:39:32 +0200

    add more test for FibonacciTest

--- week-04/day03/src/fibonacci/FibonacciTest.java
+++ week-04/day03/src/fibonacci/FibonacciTest.java
@@ -10,11 +10,29 @@ import org.junit.jupiter.api.Test;
 class FibonacciTest {
 
   int index = 12;
+  int zero = 0;
+  int one = 1;
+  int nulla;
   Fibonacci test = new Fibonacci();
 
   @Test
   void fibonacciIndex() {
     assertEquals(144, test.fibonacciIndex(index));
+  }
+
+  @Test
+  void fibonacciToZero() {
+    assertEquals(0, test.fibonacciIndex(zero));
+  }
+
+  @Test
+  void fibonacciToOne() {
+    assertEquals(1, test.fibonacciIndex(one));
+  }
+
+  @Test
+  void fibonacciToNull() {
+    assertEquals(0, test.fibonacciIndex(nulla));
   }
 
 }
