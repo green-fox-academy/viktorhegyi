dcc91ad79293abdebec9370cf44f6da41817c010
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-05 12:57:25 +0200

    add Anagram.java and TestAnagram.java

--- week-04/day03/src/anagram/Anagram.java
+++ week-04/day03/src/anagram/Anagram.java
@@ -1,8 +1,28 @@
 package anagram;
 
+import java.util.Arrays;
+
 /**
  * Created by hegyi on 2017-04-05.
  */
 public class Anagram {
 
+  public boolean testAnagram(String firstWord, String secondWord) {
+    String copyOfFirstWord = firstWord.replaceAll("\\s", "");
+    String copyOfSecondWord = secondWord.replaceAll("\\s", "");
+
+    if (copyOfFirstWord == copyOfSecondWord) {
+      return true;
+    } else if (firstWord.length() != secondWord.length()) {
+        return false;
+    } else {
+      char[] inputOneChar = copyOfFirstWord.toLowerCase().toCharArray();
+      char[] inputTwoChar = copyOfSecondWord.toLowerCase().toCharArray();
+
+      Arrays.sort(inputOneChar);
+      Arrays.sort(inputTwoChar);
+
+      return Arrays.equals(inputOneChar, inputTwoChar);
+    }
+  }
 }
---
+++ week-04/day03/src/anagram/TestAnagram.java
@@ -0,0 +1,21 @@
+package anagram;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Created by hegyi on 2017-04-05.
+ */
+public class TestAnagram {
+
+  String one = "bb d aa";
+  String two = "aa bb d";
+  Anagram test = new Anagram();
+
+  @Test
+  public void testAnagramMethod() {
+    assertEquals(true, test.testAnagram(one,two) );
+  }
+
+}
