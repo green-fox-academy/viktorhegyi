84befc84a3046793934747605bbda245b365c856
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-20 11:24:19 +0200

    update .gitignore file, add gradle project

---
+++ week-06/day-03/src/sortthearray/SortTheArray.java
@@ -0,0 +1,17 @@
+package sortthearray;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Viktor on 2017-04-19.
+ */
+public class SortTheArray {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(3,4,2,6,1));
+    ArrayList<Float> floatList = new ArrayList<>(Arrays.asList(2f, 5f, 3f, 8f));
+    ArrayList<Double> doubleList = new ArrayList<>(Arrays.asList(5.3, 4.2, 9.6, 3.2));
+    
+  }
+
+}
