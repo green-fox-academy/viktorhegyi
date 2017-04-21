fe4d7a222abe381f2551bb71078e2bf1bba7558e
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-21 09:50:26 +0200

    initial commit for weekly project todo app

---
+++ week-06/Project/src/main/java/App.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Viktor on 2017-04-21.
+ */
+public class App {
+
+  public static void main(String[] args) {
+
+    ToDoList toDoList = new ToDoList(args);
+
+    if ( args.length == 0 ) {
+      toDoList.usage();
+    } else if ( args[0].contains("-l")) {
+      toDoList.ToDoList();
+    } else if ( args[0].contains("-a")) {
+      toDoList.addTodolist();
+    } else if ( args[0].contains("-r")) {
+      toDoList.removeTodolist();
+    } else if ( args[0].contains("-c")) {
+      toDoList.checkTask();
+    } else if ( !args[0].contains("-") || args[0].contains("")) {
+      System.out.println("\nUnsupported argument\n");
+      toDoList.usage();
+    }
+  }
+
+}
---
+++ week-06/Project/src/main/java/ToDoList.java
@@ -0,0 +1,105 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Created by Viktor on 2017-04-21.
+ */
+public class ToDoList {
+
+  List<String> todolistFile = null;
+  List<String> usageFile = null;
+  Path todolistPath = Paths.get("todolist.txt");
+  String [] args;
+
+  public ToDoList(String[] args) {
+    this.args = args;
+  }
+
+  void ToDoList() {
+    try {
+      todolistFile = Files.readAllLines(todolistPath);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("Uh-oh, could not read the file!");
+    }
+    if ( todolistFile.size() == 0 ) {
+      System.out.println("No todos for today! :)");
+    } else
+      for (int i = 0; i <todolistFile.size() ; i++) {
+        System.out.println((i + 1) + " - " + todolistFile.get(i));
+      }
+  }
+
+  void usage() {
+    try {
+      Path usagePath = Paths.get("usage.txt");
+      usageFile = Files.readAllLines(usagePath);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("Uh-oh, could not read the file!");
+    }
+    for (int i = 0; i < usageFile.size(); i++) {
+      System.out.println(usageFile.get(i));
+    }
+  }
+
+  void addTodolist() {
+    todolistPath = Paths.get("todolist.txt");
+    List<String> newTask;
+
+    try {
+      if (args.length == 1) {
+        System.out.println("Unable to remove: no task provided");
+      } else {
+        newTask = Files.readAllLines(todolistPath);
+        newTask.add(newTask.size(), "[ ] " + args[1]);
+        Files.write(todolistPath,newTask);
+      }
+    } catch (Exception e) {
+      System.out.println("Uh-oh, could not read the file!");
+    }
+  }
+
+  void removeTodolist() {
+    todolistPath = Paths.get("todolist.txt");
+    List<String> removeTask;
+    try {
+      removeTask = Files.readAllLines(todolistPath);
+      if (args.length == 1) {
+        System.out.println("Unable to remove: no index provided");
+      } else if ( Integer.parseInt(args[1]) > removeTask.size()) {
+        System.out.println("Unable to remove: index is out of bound");
+      } else {
+        removeTask.remove(Integer.parseInt(args[1]) - 1);
+        Files.write(todolistPath,removeTask);
+      }
+    } catch (Exception e) {
+      System.out.println("Unable to remove: index is not a number");
+    }
+  }
+
+  void checkTask() {
+    todolistPath = Paths.get("todolist.txt");
+    List<String> checkedTask;
+    String doneTask;
+
+    try {
+      checkedTask = Files.readAllLines(todolistPath);
+      if (args.length == 1) {
+        System.out.println("Unable to check: no index provided");
+      } else if ( Integer.parseInt(args[1]) > checkedTask.size()) {
+        System.out.println("Unable to check: index is out of bound");
+      } else {
+        doneTask = checkedTask.get(Integer.parseInt(args[1]) - 1);
+        String newString = doneTask.replaceFirst(" ", "x");
+        checkedTask.set((Integer.parseInt(args[1]) - 1), newString);
+        Files.write(todolistPath, checkedTask);
+      }
+    } catch (Exception e) {
+      System.out.println("Uh-oh, could not read the file!");
+    }
+  }
+
+}
