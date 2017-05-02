40480c73173879fd720b6f39f7372e6cd538fb9c
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-02 12:04:27 +0200

    initial commit for springstart exercise

---
+++ week-07/Day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created by Viktor on 2017-05-02.
+ */
+
+@Getter
+public class Greeting {
+  long id;
+  String content;
+
+}
---
+++ week-07/Day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Viktor on 2017-05-02.
+ */
+
+@RestController
+public class HelloRESTController {
+
+  @RequestMapping(value="/greeting")
+  public Greeting greeting() {
+    Greeting newGreeting = new Greeting();
+    return newGreeting;
+  }
+
+}
