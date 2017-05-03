f218057cd2d5f0e618e69dc1a6a84da159a0679c
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-03 13:39:12 +0200

    Add exercise5

---
+++ week-07/Day-02/thymeleaf/src/main/java/com/greenfox/vik/controller/MoreBankAccountController.java
@@ -0,0 +1,31 @@
+package com.greenfox.vik.controller;
+
+import com.greenfox.vik.model.BankAccount;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Viktor on 2017-05-03.
+ */
+@Controller
+public class MoreBankAccountController {
+
+  BankAccount tiger = new BankAccount("Tigrusku", 1000, "tiger");
+  BankAccount leopard = new BankAccount("Leo", 500, "leopard");
+  BankAccount puma = new BankAccount("Pumi", 200, "puma");
+  BankAccount cat = new BankAccount("Macsek", 1, "cat");
+
+  @RequestMapping("/exercise5")
+  public String viewBankAccounts(Model model) {
+    ArrayList<BankAccount> list = new ArrayList<>();
+    list.add(0, tiger);
+    list.add(1, leopard);
+    list.add(2, puma);
+    list.add(3, cat);
+    model.addAttribute("list", list);
+    return "bankAccounts";
+  }
+
+}
