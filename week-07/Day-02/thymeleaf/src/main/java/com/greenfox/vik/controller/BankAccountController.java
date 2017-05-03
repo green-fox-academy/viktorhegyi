aa3c1ed2f6f8e9439e2366b5697c4df3880f5111
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-03 11:43:43 +0200

    Add exercise1

---
+++ week-07/Day-02/thymeleaf/src/main/java/com/greenfox/vik/controller/BankAccountController.java
@@ -0,0 +1,21 @@
+package com.greenfox.vik.controller;
+
+import com.greenfox.vik.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Viktor on 2017-05-03.
+ */
+@Controller
+public class BankAccountController {
+
+    @RequestMapping(value = "/exercise1")
+    public String viewBankAccount(Model model) {
+      BankAccount bankAccount = new BankAccount("Simba", 2000, "lion" );
+      model.addAttribute(bankAccount);
+      return "index";
+    }
+}
---
+++ week-07/Day-02/thymeleaf/src/main/java/com/greenfox/vik/model/BankAccount.java
@@ -0,0 +1,18 @@
+package com.greenfox.vik.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created by Viktor on 2017-05-03.
+ */
+
+@Getter
+@AllArgsConstructor
+public class BankAccount {
+
+  String name;
+  int balance;
+  String animalType;
+
+}
