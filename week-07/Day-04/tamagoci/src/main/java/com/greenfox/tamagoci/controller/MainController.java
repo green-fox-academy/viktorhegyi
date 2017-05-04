6b15f2af11e9538a6c418c396dd72571295d54ec
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-04 16:34:02 +0200

    Add introduction part

--- week-07/Day-04/tamagoci/src/main/java/com/greenfox/tamagoci/controller/MainController.java
+++ week-07/Day-04/tamagoci/src/main/java/com/greenfox/tamagoci/controller/MainController.java
@@ -1,8 +1,20 @@
 package com.greenfox.tamagoci.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by Viktor on 2017-05-04.
  */
+@Controller
 public class MainController {
+
+  @RequestMapping("/")
+  public ModelAndView index() {
+    ModelAndView m = new ModelAndView();
+    m.setViewName("index");
+    return m;
+  }
 
 }
