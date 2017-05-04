28128e63145b63d4194dabcd206614bddac20a0e
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-04 16:15:55 +0200

    Initial commit for week 7 project exercise

---
+++ week-07/Day-04/tamagoci/src/main/java/com/greenfox/tamagoci/TamagociApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.tamagoci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagociApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamagociApplication.class, args);
+	}
+}
---
+++ week-07/Day-04/tamagoci/src/main/java/com/greenfox/tamagoci/controller/MainController.java
@@ -0,0 +1,8 @@
+package com.greenfox.tamagoci.controller;
+
+/**
+ * Created by Viktor on 2017-05-04.
+ */
+public class MainController {
+
+}
