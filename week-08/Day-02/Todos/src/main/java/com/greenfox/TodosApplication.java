b06f30cbabd8a6b7e878bdcf18ce5cbbd4246828
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-09 12:35:09 +0200

    Add Listing Todos from embedded database

--- week-08/Day-02/Todos/src/main/java/com/greenfox/TodosApplication.java
+++ week-08/Day-02/Todos/src/main/java/com/greenfox/TodosApplication.java
@@ -1,12 +1,28 @@
 package com.greenfox;
 
+import com.greenfox.model.Todo;
+import com.greenfox.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodosApplication {
+public class TodosApplication implements CommandLineRunner {
+
+	@Autowired
+	Repository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Todo("Start the day"));
+		repository.save(new Todo("Finish H2 workshop1"));
+		repository.save(new Todo("Finsih JPA workshop2"));
+		repository.save(new Todo("Creat CRUD project"));
+
 	}
 }
--- week-08/Day-02/Todos/src/main/java/com/greenfox/controllers/TodoController.java
+++
@@ -1,8 +0,0 @@
-package com.greenfox.controllers;
-
-/**
- * Created by Viktor on 2017-05-09.
- */
-public class TodoController {
-
-}
