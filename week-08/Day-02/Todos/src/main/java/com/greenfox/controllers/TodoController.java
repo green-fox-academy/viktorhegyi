f04aa0c3fa0a3b528b22c60558b3906c43affcd8
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-09 11:04:03 +0200

    Initial commit for todo exercise W8D2

---
+++ week-08/Day-02/Todos/src/main/java/com/greenfox/controllers/TodoController.java
@@ -0,0 +1,8 @@
+package com.greenfox.controllers;
+
+/**
+ * Created by Viktor on 2017-05-09.
+ */
+public class TodoController {
+
+}
---
+++ week-08/Day-02/Todos/src/main/java/com/greenfox/model/Todo.java
@@ -0,0 +1,13 @@
+package com.greenfox.model;
+
+/**
+ * Created by Viktor on 2017-05-09.
+ */
+public class Todo {
+
+  int id;
+  String title;
+  boolean isUrgent = false;
+  boolean isDone = false;
+
+}
