724cb51929dc9c80b0f064c08a6b21f5fd9c285c
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-09 17:22:29 +0200

    Add creating function

--- week-08/Day-02/TodosMySQL/src/main/java/com/greenfox/controller/TodoController.java
+++ week-08/Day-02/TodosMySQL/src/main/java/com/greenfox/controller/TodoController.java
@@ -1,11 +1,16 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.Todo;
 import com.greenfox.repository.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Viktor on 2017-05-09.
@@ -27,4 +32,14 @@ public class TodoController {
     return "/ListTodos";
   }
 
+  @RequestMapping(value = "/add")
+  public String  newTodo() {
+    return "add";
+  }
+
+  @RequestMapping(value = "/addTodo")
+  public String  addTodo(String title, Model model) {
+    model.addAttribute("todos", repository.save(new Todo(title)));
+    return "redirect:/todo/";
+  }
 }
