e876140a6d3f820dac75a8fde0ba7c3c1c545888
Viktor Hegyi <viktorhegyi@gmail.com>
2017-05-10 09:56:30 +0200

    Initial commit for REST Backend exercise W8D3

---
+++ week-08/Day-03/REST Backend/src/main/java/com/greenfox/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.greenfox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Viktor on 2017-05-10.
+ */
+@Controller
+public class MainController {
+
+  @RequestMapping(value = "/")
+  public String index() {
+    return "index";
+  }
+
+}
