f6cf805376ea79a5b2f289f96d205c19e7e35b8c
Viktor Hegyi <viktorhegyi@gmail.com>
2017-06-01 10:47:24 +0200

    Initial commit for Calorie Counter app

---
+++ practise/src/main/java/com/greenfox/CaloriecounterApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriecounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriecounterApplication.class, args);
+	}
+}
